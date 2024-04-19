01e8d161c16872a2a7d9c708d3819f48294b5312
Joost Kaptein <joost@compositional-it.com>
2024-04-19 16:02:45 +0100

    scaffolding of build script

--- Build/Build.fs
+++ Build/Build.fs
@@ -1,3 +1,20 @@
-﻿open Fake
+﻿open Fake.Core
 
-printfn "Hello World" 
+let execContext = Context.FakeExecutionContext.Create false "build.fsx" []
+Context.setExecutionContext (Context.RuntimeContext.Fake execContext)
+
+Target.create "Bundle" (fun _ -> printfn "We are bundling!")
+
+
+
+[<EntryPoint>]
+let main args =
+    try
+        match args with
+        | [| target |] -> Target.runOrDefault target
+        | _ -> Target.runOrDefault "Bundle"
+
+        0
+    with e ->
+        printfn "%A" e
+        1
