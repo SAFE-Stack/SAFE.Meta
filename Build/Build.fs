2130952712ccd0bad2f8e882728c2d35fa4470a5
Joost Kaptein <joost@compositional-it.com>
2024-04-26 12:35:44 +0100

    add complete build step and publish step

--- Build/Build.fs
+++ Build/Build.fs
@@ -1,4 +1,5 @@
 ﻿open Fake.Core
+open Fake.Core.TargetOperators
 open Fake.DotNet
 
 let projects = [
@@ -6,11 +7,37 @@ let projects = [
     "../src/SAFE.server/SAFE.Server.fsproj"
 ]
 
+let outputFolder = "../nugetPackages"
+
 let execContext = Context.FakeExecutionContext.Create false "build.fsx" []
 Context.setExecutionContext (Context.RuntimeContext.Fake execContext)
 
-Target.create "Bundle" (fun _ -> projects |> List.map (DotNet.build id) |> ignore)
+Target.create "Bundle" (fun _ ->
+    projects
+    |> List.map (
+        DotNet.pack (fun args -> {
+            args with
+                Configuration = DotNet.BuildConfiguration.Release
+                OutputPath = Some outputFolder
+        })
+    )
+    |> ignore)
 
+
+Target.create "Publish" (fun _ ->
+    let nugetApiKey = Environment.environVarOrFail "NUGET_API_KEY"
+
+    let nugetArgs =
+        $"""push "{outputFolder}/*.nupkg" --api-key {nugetApiKey} --source https://api.nuget.org/v3/index.json"""
+
+    let result =
+        DotNet.exec (fun x -> { x with DotNetCliPath = "dotnet" }) "new" nugetArgs
+
+    if not result.OK then
+        failwithf "`dotnet %s` failed" "nuget push")
+
+
+"Bundle" ==> "Publish" |> ignore
 
 
 [<EntryPoint>]
